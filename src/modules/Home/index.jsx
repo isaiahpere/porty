20405355443e6a916b33ae3f6cb4c00357b02b8b
Heber Pereira <isaiahpereira88@gmail.com>
2023-03-15 17:39:47 -0700

    project setup

---
+++ src/modules/Home/index.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import styled from "styled-components/macro";
+
+const Container = styled.div`
+  height: 100vh;
+  width: 100vw;
+  background-color: lightblue;
+`;
+
+const Home = () => {
+  return <Container>Hello</Container>;
+};
+
+export default Home;
